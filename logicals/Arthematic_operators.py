bc90fd11a8d72df2aef4bb6248ee96a843dea321
Sravan <sravan.chindam11@gmail.com>
2023-08-04 14:27:24 +0530

    Helloworld2

---
+++ logicals/Arthematic_operators.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+#arthematic_operators
+print("{} + {} = {}".format(a,b,a+b))
+print("{} - {} = {}".format(a,b,a-b))
+print("{} * {} = {}".format(a,b,a*b))
+print("{} / {} = {}".format(a,b,a/b))
+print(  "{} % {} = {}".format(a,b,a%b))
+print(  "{} ** {} = {}".format(a,b,a**b))
+

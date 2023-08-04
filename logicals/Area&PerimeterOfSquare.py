4b73726887b8b813c4f70c385671d4577285ea01
Sravan <sravan.chindam11@gmail.com>
2023-08-04 21:36:53 +0530

    area & perimeter of a sqr

--- logicals/Area&PerimeterOfSquare.py
+++ logicals/Area&PerimeterOfSquare.py
@@ -2,7 +2,7 @@
 size = int(input("enter a size for square : "))
 #formula for calculating area of square
 area_of_sqr = size * size
-#formula for calculating area of square
+#formula for calculating perimeter of square
 peri_of_sqr = 4 * size
 print("area of a square is : ",area_of_sqr)
 print("perimeter of a square is : ",peri_of_sqr)

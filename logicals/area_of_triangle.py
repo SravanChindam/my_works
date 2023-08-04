7022b1f85ba77aa29221daa68ccb4917b9a3fdb8
Sravan <sravan.chindam11@gmail.com>
2023-08-04 20:47:59 +0530

    area of a traingle

---
+++ logicals/area_of_triangle.py
@@ -0,0 +1,7 @@
+#calculating area of a triangle
+b = float(input("enter a breadth to traingle: "))
+h = float(input("enter a height to traingle: "))
+area = 0.5*b*h
+print("area of the triangle is {}".format(area))   #using format function
+print("---------OR-----------------------------")
+print("\t",area) #used \t is for space

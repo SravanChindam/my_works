71e5c52b0b0c75975ae48ab2a9233533ae1de46c
Sravan <sravan.chindam11@gmail.com>
2023-08-06 00:14:43 +0530

    sub usages

---
+++ logicals/ToKnowUsageOfSub.py
@@ -0,0 +1,11 @@
+sub_name=input("enter name of a subject : ")
+if (sub_name == "python"):
+    print("{} for datascience applications".format(sub_name))
+elif (sub_name == "java"):
+    print("{} for web applications development".format(sub_name))
+elif (sub_name == "c++"):
+    print("{} for telecommunication applications".format(sub_name))
+elif (sub_name == "c language"):
+    print("{} for OS development".format(sub_name))
+else:
+    print("No Idea")

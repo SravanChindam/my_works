8acb48a5b910eda14158407b9c01cdb9cd97bedd
Sravan <sravan.chindam11@gmail.com>
2023-08-05 16:42:37 +0530

    big num

---
+++ logicals/FindBiggestNumByUsingTernaryOp.py
@@ -0,0 +1,5 @@
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+#Taking biggest value as bv
+bv = a if a>b else b if b>a else "VALUES ARE EQUAL"
+print("Biggest numer is --->",bv)

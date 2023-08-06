535117afdb9a4a2c76b9c273f8b477aff2169685
Sravan <sravan.chindam11@gmail.com>
2023-08-07 00:38:06 +0530

    inner loop1

---
+++ logicals/InnerLoopsConcept.py
@@ -0,0 +1,7 @@
+for i in range(1,6):
+    print("-" * 30)
+    print("the value of i outer loop {}".format(i))
+    print("-"*30)
+    for j in range(1,4):
+        print("the value of j inner loop {}".format(j))
+

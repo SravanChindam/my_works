689beb18048b85a1026467af8a70f918936a483b
Sravan <sravan.chindam11@gmail.com>
2023-08-18 12:49:27 +0530

    Checking Leap year or Not

--- logicals/CheckLeapYear.py
+++ logicals/CheckLeapYear.py
@@ -1,4 +1,5 @@
 while(True):             #if you want to try again and again
+    #Using Function for checking leap year or not
     def check_year(year):
         return((year%4==0) and (year%100!=0)or(year%400==0))
     year = int(input("enter a year: "))
@@ -9,6 +10,9 @@ while(True):             #if you want to try again and again
         print("not a Leap year")
 
     year = int(input("enter a year: "))
+
+    #Using if condition for checking leap year or not
+
     if (year%4==0) and (year%100!=0) or (year%400==0):
         print("{} is Leap year".format(year))
     else:

1b2f2d09f5840f8c928a6a5177177de4a0fe1b4f
Sravan <sravan.chindam11@gmail.com>
2023-09-02 02:08:37 +0530

    class ex2

---
+++ logicals/OOpsEx2.py
@@ -0,0 +1,9 @@
+class account:
+    def ______int__(self):
+        self.acno=62226532541
+        self.Cust_name="sravan"
+        self.balance=78200.00
+        self.pin=3762
+        self.bank_name="SBI"
+
+
---
+++ logicals/OOpsEx2_import1.py
@@ -0,0 +1,8 @@
+from OOpsEx2 import account
+ac=account()
+ac.______int__()
+print("Account Number:{}".format(ac.acno))
+print("Account Holder Name:{}".format(ac.Cust_name))
+print("Account Balance:{}".format(ac.balance))
+print("Account Pin:{}".format(ac.pin))
+print("Account Branch Name:{}".format(ac.bank_name))
